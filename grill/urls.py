6ff024ba59bdea2b7cdd37cf0eecf83e494d7d2a
AOrlov5707126 <zhiraforl@ya.ru>
2017-03-06 22:21:13 -0800

    Map, geolocation, markers, GL bug fixed

--- grill/urls.py
+++ grill/urls.py
@@ -1,4 +1,9 @@
-"""grill URL Configuration
+#!python
+# -*- coding: utf8 -*-
+
+""" Адреса уровня проекта
+
+grill URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -13,9 +18,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^sample/', include('sample.urls')),
+    url(r'^b2c/', include('b2c.urls')),
 ]
