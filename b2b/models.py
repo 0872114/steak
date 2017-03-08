881156c1ff60641f7d710c87180e4902dfbf34c5
0872114 <0872114@gmail.com>
2017-03-08 08:08:51 +0500

    moved Printers to b2b. Added sample verbose names

--- b2b/models.py
+++ b2b/models.py
@@ -1,8 +1,12 @@
+#!python
+# -*- coding: utf8 -*-
+
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Printer(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(_(u'Название'), max_length=30)
     address = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
@@ -10,6 +14,10 @@ class Printer(models.Model):
     services = models.CharField(max_length=50)
     lat = models.FloatField()
     lon = models.FloatField()
+
+    class Meta:
+        verbose_name = _(u'Печатник')
+        verbose_name_plural = _(u'Печатники')
 
     def __unicode__(self):
         return u'%s %s' % (self.name, self.email)
