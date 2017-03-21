d80792be23870a3deee5a8cbaa1d54974079e885
qed228 <tpl77@yandex.ru>
2017-03-21 06:43:10 -0700

    Templates for Register and Login in Django.

--- logit/views.py
+++ logit/views.py
@@ -1,20 +1,13 @@
-from django.shortcuts import render_to_response, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib import auth
-from django.template.context_processors import csrf
+from forms import MyUserCreationForm
+from django.shortcuts import render_to_response
+
+
+def login(request):
+    form = MyUserCreationForm()
+    return render_to_response('logit/login.html', {'form': form})
 
 
 def register(request):
-    args = {}
-    args.update(csrf(request))
-    args['form'] = UserCreationForm()
-    if request.POST:
-        newuser_form = UserCreationForm(request.POST)
-        if newuser_form.is_valid():
-            newuser_form.save()
-            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password2'])
-            auth.login(request, newuser)
-            return redirect('/')
-        else:
-            args['form'] = newuser_form
-    return render_to_response('registration/register.html', args)
+    form = MyUserCreationForm()
+    return render_to_response("logit/register.html", {
+        'form': form})
