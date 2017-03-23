8ebe78c0b796265682d179c6dc67a548be746747
qed228 <tpl77@yandex.ru>
2017-03-23 08:43:20 -0700

    Corret urls and base template. Now all links should work fine.

--- b2c/urls.py
+++ b2c/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from views import *
 
 urlpatterns = [
-    url(r'^$', map, name="b2c"),
-    url('^market/', market, name="b2c"),
+    url(r'^$', map, name="map"),
+    url('^market/', market, name="market"),
 ]
